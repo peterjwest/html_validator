e70ad0ab44462b49b6d64c9f18f2ab796995006a
Peter West <peterjwest3@gmail.com>
2011-05-26 02:36:08 +0100

    Added tag group for script/style

--- html_4.01_spec.js
+++ html_4.01_spec.js
@@ -9,6 +9,7 @@ var html_401_spec = function(doctype) {
       tags: {
         all: 'a,abbr,acronym,address,area,b,base,bdo,big,blockquote,body,br,button,caption,cite,code,col,colgroup,dd,del,dfn,div,dl,dt,em,fieldset,form,h1,h2,h3,h4,h5,h6,head,hr,html,i,img,input,ins,kbd,label,legend,li,link,map,meta,noscript,object,ol,optgroup,option,p,param,pre,q,#root,samp,script,select,small,span,strong,style,sub,sup,table,tbody,td,textarea,tfoot,th,thead,title,tr,tt,ul,var',
         block: 'heading,pre,p,dl,div,noscript,blockquote,form,hr,table,fieldset,address',
+        cdata_elements: 'script,style',
         close_optional: 'body,colgroup,dd,dt,head,html,li,option,p,tbody,td,tfoot,th,thead,tr',
         document_body: 'body',
         flow: 'inline,block',
